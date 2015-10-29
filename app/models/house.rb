1272f756348c408b43c02eac9fd25ac1c36f8377
Yayamalita <siya.nyinfo@gmail.com>
2015-10-29 15:40:51 +0700

    Plan spec

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,5 +1,6 @@
 class House < ActiveRecord::Base
   belongs_to :type
+  has_many :plan
   validates_presence_of :name, :bedroom, :bathroom, :width, :height, :depth,
                         :cost, :floorArea, :buildingArea
 end
