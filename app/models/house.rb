cfce409ab5be847caebcf732fff1224a28afed69
Yayamalita <siya.nyinfo@gmail.com>
2015-10-29 16:36:19 +0700

    House Spec

--- app/models/house.rb
+++ app/models/house.rb
@@ -3,4 +3,7 @@ class House < ActiveRecord::Base
   has_many :plan
   validates_presence_of :name, :bedroom, :bathroom, :width, :height, :depth,
                         :cost, :floorArea, :buildingArea
+  validates_length_of :name, in: 5..25
+  validates_inclusion_of :bedroom, in: 1..100
+  validates_inclusion_of :bathroom, in: 1..100
 end
--- spec/models/house_spec.rb
+++ spec/models/house_spec.rb
@@ -8,9 +8,15 @@ describe House do
 
   it { is_expected.to validate_presence_of(:name) }
 
+  it { is_expected.to validate_length_of(:name).is_at_least(5).is_at_most(25) }
+
   it { is_expected.to validate_presence_of(:bedroom) }
 
+  it { is_expected.to validate_inclusion_of(:bedroom).in_range(1..100) }
+
   it { is_expected.to validate_presence_of(:bathroom) }
+
+  it { is_expected.to validate_inclusion_of(:bathroom).in_range(1..100) }
 
   it { is_expected.to validate_presence_of(:width) }
 
